0ca2ae9beeccfbe2531d66a3c489313dcfa691ab
Mehakpreet21 <mehakpreetkaur2001@gmail.com>
2022-03-01 13:42:12 -0500

    added all exam pages

--- OnlineGrading/settings.py
+++ OnlineGrading/settings.py
@@ -32,13 +32,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'authentication.apps.AuthenticationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authentication.apps.AuthenticationConfig',
     'exam.apps.ExamConfig',
     'teacher.apps.TeacherConfig',
     'student.apps.StudentConfig',
@@ -82,7 +82,7 @@ if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': str(BASE_DIR / 'db.sqlite3'),
         }
     }
 else:
