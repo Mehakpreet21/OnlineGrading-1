336e48e3e0e2e4c292be3c089c3cc0144e0b3b41
Mayaank Vadlamani <mayaankvad@gmail.com>
2022-03-06 00:02:14 -0500

    db

--- OnlineGrading/settings.py
+++ OnlineGrading/settings.py
@@ -78,13 +78,25 @@ WSGI_APPLICATION = 'OnlineGrading.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        #postgres://zaagsnihgpslrj:0484b257a4dd54e429fcc3bc7b80f50b187c7fbdc130278ac153b11868802eb8@ec2-3-225-79-57.compute-1.amazonaws.com:5432/d3pdoif4q6hmjf
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'd3pdoif4q6hmjf',
+            'HOST': 'ec2-3-225-79-57.compute-1.amazonaws.com',
+            'PORT': 5432,
+            'USER': 'zaagsnihgpslrj',
+            'PASSWORD': '0484b257a4dd54e429fcc3bc7b80f50b187c7fbdc130278ac153b11868802eb8'
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
